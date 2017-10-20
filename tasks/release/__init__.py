09f5e1f9923de37a96b396399293843f322712b9
Pradyun Gedam <pradyunsg@gmail.com>
2017-10-20 11:54:07 +0530

    task: :new: release.check

--- tasks/release/__init__.py
+++ tasks/release/__init__.py
@@ -2,3 +2,21 @@
 """
 
 from .bump_version import bump_version
+from .utils import find_version, version_locations
+
+from invoke import task
+
+
+@task
+def check(ctx):
+    # Check that the version in the various locations are in sync
+    versions = {
+        find_version(*parts) for parts in version_locations
+    }
+
+    if len(versions) != 1:
+        raise Exception(
+            "Got version mismatch: {}".format(",".join(sorted(versions)))
+        )
+    else:
+        print("[release.check] version strings are in sync.")
